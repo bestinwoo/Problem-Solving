17352237976042d497f0bdb55451f9048bd5c86e
bestinwoo <bestinwoo@gmail.com>
2021-08-16 19:23:11 +0900

    [BOJ] 1436번: 영화감독 숌

---
+++ 백준/1436.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <queue>
+/*
+* 분류 : 브루트포스 알고리즘
+* i를 666부터 차례로 증가시켜 666이 포함될 때마다 cnt를 더해주고
+* cnt와 n이 같아지면 값을 출력
+*/
+using namespace std;
+
+int main(void)
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int n, cnt = 0, i;
+	cin >> n;
+
+	for (i = 666;;i++) {
+		string s = to_string(i);
+		if (s.find("666") != string::npos) cnt++;
+		if (cnt == n) {
+			cout << i;
+			break;
+		}
+	}
+	return 0;
+}
