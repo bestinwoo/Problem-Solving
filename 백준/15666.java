c482ffa1662b55be37b5d19bc22517c6621b3334
bestinwoo <bestinwoo@gmail.com>
2022-08-06 00:01:23 +0900

    [BOJ] N과 M (12)

---
+++ 백준/15666.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n,m;
+	static int[] arr;
+	static int[] nums;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		arr = new int[m];
+		nums = new int[n];
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st1.nextToken());
+		}
+		Arrays.sort(nums);
+		dfs(0,0);
+		System.out.println(sb);
+
+	}
+
+	public static void dfs(int index, int cnt) {
+		if(cnt == m) {
+			for(int i = 0; i < m; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append('\n');
+			return;
+		}
+		int prev = 0;
+		for (int i = index; i < n; i++) {
+			if(prev != nums[i]) {
+				arr[cnt] = nums[i];
+				prev = arr[cnt];
+				dfs(i, cnt + 1);
+			}
+		}
+	}
+}
