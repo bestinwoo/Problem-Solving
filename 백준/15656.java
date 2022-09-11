b566ee32d4f70a1836af6f7914d1c629c13c252c
bestinwoo <bestinwoo@gmail.com>
2022-09-12 04:54:28 +0900

    [BOJ] N과 M (7)

---
+++ 백준/15656.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+//BOJ 15656 N과 M (7)
+public class Main {
+	static int n, m;
+	static int[] selected, arr;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		n = Integer.parseInt(input[0]);
+		m = Integer.parseInt(input[1]);
+		arr = new int[n];
+		selected = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		dfs(0);
+		System.out.println(sb);
+	}
+
+	public static void dfs(int cnt) {
+		if(cnt == m) {
+			for(int i = 0; i < cnt; i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append('\n');
+			return;
+		}
+
+		for(int i = 0; i < n; i++) {
+				selected[cnt] = arr[i];
+				dfs(cnt+1);
+		}
+	}
+}
+
