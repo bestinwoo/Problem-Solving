1e867fdc027598e44ce1cc91b41f1d1276e3dc4a
bestinwoo <bestinwoo@gmail.com>
2022-10-26 00:37:27 +0900

    [LeetCode] Merge Two Sorted Lists

---
+++ LeetCode/Merge Two Sorted Lists.java
@@ -0,0 +1,30 @@
+//LeetCode Merge Two Sorted Lists
+class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        ListNode merged = new ListNode();
+        ListNode cursor = merged;
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                cursor.next = list1;
+                list1 = list1.next;
+            } else {
+                cursor.next = list2;
+                list2 = list2.next;
+            }
+            cursor = cursor.next;
+        }
+        cursor.next = list1 == null ? list2 : list1;
+        return merged.next;
+    }
+}
