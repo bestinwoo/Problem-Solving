08e7157f7b247241b0c8c6008bbfdc1242e856c1
bestinwoo <bestinwoo@gmail.com>
2022-11-25 23:58:57 +0900

    [LeetCode] Add Binary

---
+++ LeetCode/Add Binary.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+import java.util.Stack;
+//LeetCode Add Binary
+class Solution {
+	public String addBinary(String a, String b) {
+		BigInteger a1 = new BigInteger(a, 2);
+		BigInteger b1 = new BigInteger(b, 2);
+		return a1.add(b1).toString(2);
+	}
+}
